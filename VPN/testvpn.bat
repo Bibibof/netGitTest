4fe3d4b4e8fcf6c52e136d20d46cba6a209a38ba
Bibibof <bibibof@hotmail.com>
2015-05-25 14:35:09 +0800

    Update testvpn.bat

--- VPN/testvpn.bat
+++ VPN/testvpn.bat
@@ -9,8 +9,15 @@ goto :eof
 :CheckEmpty
 @rem echo Test if can reach the station...
 @rem echo %~z1
-if %~z1 == 0 goto :eof
+if %~z1 == 0 goto :saveip
 @rem echo Cannot reach the station...
 start /B d:\GitHub\netGitTest\VPN\cisco.ahk
 goto :eof
+:saveip
+d:
+cd d:\GitHub\netGitTest
+C:\Users\rorain\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\cmd\git add -A
+C:\Users\rorain\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\cmd\git commit -m "ip"
+C:\Users\rorain\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\cmd\git push
+:eof
 exit
